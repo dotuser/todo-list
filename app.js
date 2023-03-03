6940ddfd65c52e8d6608485ee009b2d6f0a45032
dotuser <ahmedhassaan@tutanota.com>
2023-03-03 20:13:10 +0500

    EJS Templates

--- app.js
+++ app.js
@@ -4,8 +4,20 @@ const bodyParser = require('body-parser');
 const app = express();
 const PORT = process.env.PORT || 4444;
 
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
 app.get('/', (req, res) => {
-    res.send("Home Page");
+    let today = new Date();
+    let options = {
+        weekday: "long",
+        month: "short",
+        day: "numeric"
+    }
+    let day = today.toLocaleString("en-US", options);
+
+    res.render('list', {day: day});
 });
 
 app.listen(PORT, () => {
